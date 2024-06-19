ba56ebf82ba1eceac477567fe1b5564e0450e246
YxngXi1 <yang@xue.email>
2024-06-18 22:31:48 -0400

    Completed first part of footer, will need to adust other portions later

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>this should be at the bottom hopefully if i can code good</div>
+    <main className='h-screen w-screen'>
+      <div className='custom-background-2 w-full h-3/6 justify-center items-center flex flex-col'>
+        <p className='text-amber-400 tracking-widest'>SUBSCRIBE</p>
+        <p className='text-white text-5xl font-bold my-10 text-center'>John Fraser SAC Google Classroom</p>
+        <p className='text-white text-xl text-center font-light'>Receive monthly updates and pictures of our events!</p>
+        <a href='https://classroom.google.com/u/0/c/NDQ0MDU4NTQxNDBa?cjc=i725i36' target='_blank' className='my-10'>
+          <button className='button-4'>
+            <p>Join Now</p>
+          </button>
+        </a>
+      </div>
+      <div className='bg-black h-full'>
+
+      </div>
+    </main>
   )
 }
 
