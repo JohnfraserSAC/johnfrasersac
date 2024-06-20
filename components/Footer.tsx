3435fe376ce1e38235b7fadaddf4ce9b0c980671
YxngXi1 <yang@xue.email>
2024-06-20 12:57:59 -0400

    finishing touches on footer (incomplete)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <main className='h-screen w-screen flex flex-col'>
-      <div className='custom-background-2 flex justify-center items-center flex-col h-5/12'>
+      <div className='custom-background-2 flex justify-center items-center flex-col h-7/12'>
         <p className='text-amber-400 tracking-widest text-xs'>SUBSCRIBE</p>
         <p className='text-white text-5xl font-semibold my-4 text-center leading-tight'>John Fraser SAC Google Classroom</p>
         <p className='text-white text-xl text-center font-light'>Receive monthly updates and pictures of our events!</p>
@@ -14,24 +14,24 @@ const Footer = () => {
           </button>
         </a>
       </div>
-      <div className='bg-black flex flex-col justify-center items-center text-center h-7/12'>
+      <div className='bg-black flex flex-col justify-center items-center text-center '>
         <div className=''>
           <div className='w-full flex justify-center items-center text-center my-4'>
             <Image
                   className=''
                   src='/WSAC-Logo.png' 
                   alt='logo' 
-                  width={200}
-                  height={366} />
+                  width={225}
+                  height={1} />
           </div>
-            <p className='text-white tracking-widest font-normal text-base '>Questions? Shoot us an email at johnfraserstudentcouncil@gmail.com or message @johnfrasersac on Instagram. For questions or inquiries about clubs, please email info@johnfrasersac.com</p>
+            <p className='text-white tracking-widest font-light text-base mx-52'>Questions? Shoot us an email at johnfraserstudentcouncil@gmail.com or message @johnfrasersac on Instagram. For questions or inquiries about clubs, please email info@johnfrasersac.com</p>
         </div>
         <div className="w-full">
           <hr className='hr-2'></hr>
         </div>
-        <div className=''>
-          <p className='text-white mb-10'>Copyright symbol of copyright SAC John Fraser</p>
-          <p className='text-white'>Made and Maintained By John Fraser Students</p>
+        <div className='lg:flex lg:justify-evenly lg:w-full'>
+          <p className='text-white mb-8'>Copyright &copy; SAC John Fraser</p>
+          <p className='text-white mb-2'>Made and Maintained By John Fraser Students</p>
         </div>
       </div>
     </main>
