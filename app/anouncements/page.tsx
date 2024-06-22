5a5c437fc94611a49a1b8969f7399f6360726df3
YxngXi1 <yang@xue.email>
2024-06-22 18:29:09 -0400

    brought back navbar

--- app/anouncements/page.tsx
+++ app/anouncements/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Image from 'next/image'
 
 const page = () => {
   return (
-    <div>anoucnements</div>
+    <main>
+      <div>
+        <Image
+          src='/placeholder.jpg'
+          height={100}
+          width={100}
+          alt='placeholder'
+          />
+        <h1>Anouncements</h1>
+        <p></p>
+      </div>
+    </main>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       
       <body>
-        {/* <Header /> */}
+        <Header />
         <AOSInitializer />
           {children}
         <Footer />
