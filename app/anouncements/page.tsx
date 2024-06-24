cd53e3015e4d42ac0e3b6117c100f5145c1d73f5
YxngXi1 <yang@xue.email>
2024-06-23 21:53:16 -0400

    got started on anouncements, created new component for background, background art design in need of higher quality

--- app/anouncements/page.tsx
+++ app/anouncements/page.tsx
@@ -1,24 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
+import AnouncementHeroBackground from '@/components/anouncements-bg'
 
 const page = () => {
   return (
     <main>
-      <div>
-        <Image
-          src='/placeholder.jpg'
-          height={100}
-          width={100}
-          alt='placeholder'
-          />
-        <h1>Anouncements</h1>
-        <p>CLick here for daily anouncements and events to come</p>
-      </div>
-      <div>
-        <a>
-          <p></p>
-        </a>
-      </div>
+      <AnouncementHeroBackground />
     </main>
   )
 }
---
+++ components/anouncements-bg.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+
+const anouncementHeroBackground = () => {
+  return (
+    <div className="relative custom-background-3 h-screen"> {/* Adjust the height as needed */}
+        <div className="absolute top-1/2 left-0 transform -translate-y-1/2">
+          <Image
+            src="/cw-1.png"
+            alt="Left Image"
+            width={500} 
+            height={300} 
+            objectFit="cover"
+          />
+        </div>
+        <div className="absolute bottom-0 right-0">
+          <Image
+            src="/cw-2.png"
+            alt="Right Image"
+            width={500} 
+            height={300} 
+            objectFit="cover" 
+          />
+        </div>
+      </div>
+  )
+}
+
+export default anouncementHeroBackground
