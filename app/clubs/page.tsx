ca3519ae37444a983d2ea04640633000ec9e4c09
YxngXi1 <yang@xue.email>
2024-07-02 23:02:24 -0400

    started work on clubs page

--- app/clubs/page.tsx
+++ app/clubs/page.tsx
@@ -1,8 +1,45 @@
+"use client"
+
 import React from 'react'
+import clubs from '@/components/clubs'
+import { useState } from 'react';
+import Link from 'next/link';
 
 const page = () => {
+  const [sortOrder, setSortOrder] = useState('ascending'); // Default to 'ascending'
+
+  // Correctly sort clubs based on sortOrder
+  const sortedAnnouncements = clubs.sort((a, b) => {
+    if (sortOrder === 'ascending') {
+      return a.name.localeCompare(b.name); // For A-Z
+    } else {
+      return b.name.localeCompare(a.name); // For Z-A
+    }
+  });
+
   return (
-    <div>clubs</div>
+    <main>
+      <div className='h-96 flex justify-center items-center flex-col text-center'>
+        <h1>Clubs</h1>
+        <p>Check here for all the updates and clubs here at John Fraser Secondary School!</p>
+      </div>
+      <div className='w-full flex justify-center items-center'>
+        <div id='filter' className=' w-7/12 flex justify-center items-center mb-28'>
+          <button className='button-2' onClick={() => setSortOrder('ascending')}>A-Z</button>
+          <button className='button-2' onClick={() => setSortOrder('descending')}>Z-A</button>
+        </div>
+      </div>
+      <div className='flex justify-center items-center flex-col mb-12'>
+        {sortedAnnouncements.map((club) => (
+          <div key={club.id} className="asd123">
+            <h2>{club.name}</h2>
+            <p>{club.description}</p>
+            <Link href='/clubs/'
+            <button className='button-5'>a</button>
+          </div>
+        ))}
+      </div>
+    </main>
   )
 }
 
---
+++ components/clubs.js
@@ -0,0 +1,20 @@
+const clubs = [
+    {
+        id: 1,
+        name: 'HOSA',
+        description: 'Description for Club 1',
+    },
+    {
+        id: 2,
+        name: 'SAC',
+        description: 'Description for Club 2',
+    },
+    {
+        id: 3,
+        name: 'FAC',
+        description: 'Description for Club 3',
+        insta: '',
+    },
+];
+
+export default clubs;
