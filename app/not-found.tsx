79be0989678c74afb4f5f3610900c397061c9930
YxngXi1 <yang@xue.email>
2024-07-03 16:41:02 -0400

    fun 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+ 
+export default async function NotFound() {
+  return (
+    <main className='h-screen'>
+        <div className='h-full flex justify-center items-center flex-col text-center'>
+            <h1 className='text-4xl font-bold'>Whoops!</h1>
+            <h2 className='text-2xl'>Looks like you slipped up!</h2>
+            <Image
+                src='/dog-slip.jpg'
+                height={400}
+                width={400}
+                alt='dog slipping'
+                    />
+            <p className='text-lg'>Here's the way <a href='/' style={{ color: 'blue', textDecoration: 'underline' }}>home</a></p>
+        </div>
+    </main>
+  )
+}
