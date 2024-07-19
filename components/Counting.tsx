d9dc6de6709e66be7557865200c3c61158685e25
YxngXi1 <yang@xue.email>
2024-07-19 16:21:20 -0400

    finished mobile version and working on desktop version

--- components/Counting.tsx
+++ components/Counting.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-const easeOutQuad = (t: number) => t * ( 2 - t );
+const easeOutQuint = (t: number) => 1 + (--t) * t * t * t * t;
 const frameDuration = 1000 / 60;
 
 const CountUpAnimation = ( { children, duration = 2000 } ) => {
@@ -12,7 +12,7 @@ const CountUpAnimation = ( { children, duration = 2000 } ) => {
 		const totalFrames = Math.round( duration / frameDuration );
 		const counter = setInterval( () => {
 			frame++;
-			const progress = easeOutQuad( frame / totalFrames );
+			const progress = easeOutQuint( frame / totalFrames );
 			setCount( countTo * progress );
 
 			if ( frame === totalFrames ) {
