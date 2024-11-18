4cca813c2cc980221c2002525ace2f6628424f8b
arya holmukhe <aryaholmukhe@gmail.com>
2024-11-18 08:30:28 -0500

    updated 404 page for formality

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,15 +8,15 @@ export default function NotFound() {
   return (
     <main className='h-screen'>
         <div className='h-full flex justify-center items-center flex-col text-center'>
-            <h1 className='text-4xl font-bold'>Whoops!</h1>
-            <h2 className='text-2xl'>Looks like you slipped up!</h2>
+            <h1 className='text-4xl font-bold'>Error 404</h1>
+            <h2 className='text-2xl'>Page not found</h2>
             <Image
-                src='/dog-slip.jpg'
-                height={400}
-                width={400}
+                src='/john-fraser-logo.png'
+                height={300}
+                width={200}
                 alt='dog slipping'
                     />
-            <p className='text-lg'>Here&apos;s the way <Link href='/' className='text-blue-500 underline'>home</Link></p>
+            <p className='text-lg'><Link href='/' className='bg-blue-200 text-blue-950 underline p-3'>Go Back</Link></p>
         </div>
     </main>
   )
