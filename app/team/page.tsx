fd8008d87a170f46fc8ca380be58cdad11125693
arya holmukhe <aryaholmukhe@gmail.com>
2024-12-08 01:06:14 -0500

    added entire sac team + layout bug fix

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -7,100 +7,261 @@ const teamMembers = [
     id: 1,
     name: "Aleena Ali",
     role: "President",
-    image: "/team/aleena-ali.jpg",
-    description: "Hi Jags! I’m Aleena, one of your Co-Presidents for this year :) The last 3 years on SAC have been amazing and we can’t wait to introduce you all to all of the amazing events this year!",
+    image: "/team/execs/aleena-ali.jpg",
+    description:
+      "Hi Jags! I’m Aleena, one of your Co-Presidents for this year :) The last 3 years on SAC have been amazing and we can’t wait to introduce you all to all of the amazing events this year!",
   },
   {
     id: 2,
     name: "Cody Doan",
     role: "President",
-    image: "/team/cody-doan.jpg",
-    description: "Hey everyone! I'm Cody, one of your Co-Presidents this year! After three years on SAC, I'm excited about the amazing events we have planned and look forward to making this year truly memorable for all of us!",
+    image: "/team/execs/cody-doan.jpg",
+    description:
+      "Hey everyone! I'm Cody, one of your Co-Presidents this year! After three years on SAC, I'm excited about the amazing events we have planned and look forward to making this year truly memorable for all of us!",
   },
-    {
+  {
     id: 3,
     name: "Shashvati Deshmukh",
     role: "Secretary",
-    image: "/team/shashvati-deshmukh.jpg",
+    image: "/team/execs/shashvati-deshmukh.jpg",
     description: "",
   },
   {
     id: 4,
     name: "Nikhil Khanna",
     role: "Treasurer",
-    image: "/team/nikhil-khanna.jpg",
-    description: "Hey everyone!  I’m Nikhil, your treasurer for this year.  I’m super excited to work with the student body and SAC to ensure our events are as fun as possible (while staying in budget 🤑)!!!",
+    image: "/team/execs/nikhil-khanna.jpg",
+    description:
+      "Hey everyone!  I’m Nikhil, your treasurer for this year.  I’m super excited to work with the student body and SAC to ensure our events are as fun as possible (while staying in budget 🤑)!!!",
   },
   {
     id: 5,
     name: "Amrita Rajaram",
     role: "Clubs Liaison",
-    image: "/team/amrita-rajaram.jpg",
-    description: "Hi everyone! I'm Amrita and your club liaison this year! If you're interested in anything clubs-related or want some info/clarity on any club matter reach out to me through email or Instagram! Very excited to see what all the clubs accomplish this year!",
+    image: "/team/execs/amrita-rajaram.jpg",
+    description:
+      "Hi everyone! I'm Amrita and your club liaison this year! If you're interested in anything clubs-related or want some info/clarity on any club matter reach out to me through email or Instagram! Very excited to see what all the clubs accomplish this year!",
   },
   {
     id: 6,
     name: "Arya Holmukhe",
     role: "Technology Liaison",
-    image: "/team/arya-holmukhe.jpg",
+    image: "/team/execs/arya-holmukhe.jpg",
     description: "",
   },
   {
     id: 7,
     name: "Akshat Chopra",
     role: "Environmental Liaison",
-    image: "/team/akshat-chopra.jpg",
+    image: "/team/execs/akshat-chopra.jpg",
     description: "",
   },
   {
     id: 8,
     name: "Ronia Puliyelil",
     role: "Arts Liaison",
-    image: "/team/ronia-puliyelil.jpg",
+    image: "/team/execs/ronia-puliyelil.jpg",
     description: "",
   },
-  
+
   {
     id: 9,
     name: "Hamza Saleh",
     role: "Athletics Liaison",
-    image: "/team/hamza-saleh.jpg",
+    image: "/team/execs/hamza-saleh.jpg",
     description: "",
   },
   {
     id: 10,
     name: "Inesh Haque",
     role: "Community Outreach",
-    image: "/team/inesh-haque.jpg",
+    image: "/team/execs/inesh-haque.jpg",
     description: "",
   },
   {
     id: 11,
     name: "Bismaad Kalra",
     role: "Social Convenor",
-    image: "/team/bismaad-kalra.jpg",
+    image: "/team/execs/bismaad-kalra.jpg",
     description: "",
   },
   {
     id: 12,
     name: "Cindy Lee",
     role: "Photography Executive",
-    image: "/team/cindy-lee.jpg",
+    image: "/team/execs/cindy-lee.jpg",
     description: "",
   },
   {
     id: 13,
     name: "Kirpa Chandi",
     role: "Promotions Officer",
-    image: "/team/kirpa-chandi.jpg",
+    image: "/team/execs/kirpa-chandi.jpg",
     description: "",
   },
   {
-    id: 14  ,
+    id: 14,
     name: "Adolina Kiflay",
     role: "Promotions Officer",
-    image: "/team/adolina-kiflay.jpg",
-    description: "Hey jags! I’m Adolina Kiflay and I’m one of your promotions officer this year and I’m so excited to make new and engaging promo with all of you!",
+    image: "/team/execs/adolina-kiflay.jpg",
+    description:
+      "Hey jags! I’m Adolina Kiflay and I’m one of your promotions officer this year and I’m so excited to make new and engaging promo with all of you!",
+  },
+];
+
+const gradeReps = [
+  {
+    id: 1,
+    name: "Shaurya Dedhia",
+    role: "Grade 9 Rep.",
+    image: "/team/grade-reps/shaurya-dedhia.jpg",
+  },
+  {
+    id: 2,
+    name: "Akshath Suresh",
+    role: "Grade 10 Rep.",
+    image: "/team/grade-reps/akshath-suresh.jpg",
+  },
+  {
+    id: 3,
+    name: "Tianna Lim",
+    role: "Grade 11 Rep.",
+    image: "/team/unknown.jpg",
+  },
+  {
+    id: 4,
+    name: "Eshal Siddiqi",
+    role: "Treasurer",
+    image: "/team/grade-reps/eshal-siddiqi.jpg",
+  },
+];
+
+const honouraries = [
+  {
+    id: 1,
+    name: "Aaradhana Vijaykrishnan",
+    role: "Honoroury",
+    image: "/team/honorouries/aaradhana-vijaykrishnan.jpg",
+  },
+  {
+    id: 2,
+    name: "Abdelrahman",
+    role: "Honoroury",
+    image: "/team/honorouries/abdelrahman.jpg",
+  },
+  {
+    id: 3,
+    name: "Ananya Sithique Hasan",
+    role: "Honoroury",
+    image: "/team/honorouries/ananya-sithique-hasan.jpg",
+  },
+  {
+    id: 4,
+    name: "Anthony Doan",
+    role: "Honoroury",
+    image: "/team/honorouries/anthony-doan.jpg",
+  },
+  {
+    id: 5,
+    name: "Arjun Garg",
+    role: "Honoroury",
+    image: "/team/honorouries/arjun-garg.jpg",
+  },
+  {
+    id: 6,
+    name: "Arshaan Thind",
+    role: "Honoroury",
+    image: "/team/honorouries/arshaan-thind.jpg",
+  },
+  {
+    id: 7,
+    name: "David Chen",
+    role: "Honoroury",
+    image: "/team/honorouries/david-chen.jpg",
+  },
+  {
+    id: 8,
+    name: "Isabelle Ng",
+    role: "Honoroury",
+    image: "/team/honorouries/isabelle-ng.jpg",
+  },
+  {
+    id: 9,
+    name: "Jamie Chen",
+    role: "Honoroury",
+    image: "/team/honorouries/jamie-chen.jpg",
+  },
+  {
+    id: 10,
+    name: "Justin Deng",
+    role: "Honoroury",
+    image: "/team/honorouries/justin-deng.jpg",
+  },
+  {
+    id: 11,
+    name: "Muhammad Syed",
+    role: "Honoroury",
+    image: "/team/honorouries/muhammad-yed.jpg",
+  },
+  {
+    id: 12,
+    name: "Nadine Mughal",
+    role: "Honoroury",
+    image: "/team/honorouries/nadine-mughal.jpg",
+  },
+  {
+    id: 13,
+    name: "Nolan Lee",
+    role: "Honoroury",
+    image: "/team/honorouries/nolan-lee.jpg",
+  },
+  {
+    id: 14,
+    name: "Sami Khan",
+    role: "Honoroury",
+    image: "/team/honorouries/sami-khan.jpg",
+  },
+  {
+    id: 15,
+    name: "Shanvir Sidhu",
+    role: "Honoroury",
+    image: "/team/honorouries/shanvir-sidhu.jpg",
+  },
+  {
+    id: 16,
+    name: "Sophia Chen",
+    role: "Honoroury",
+    image: "/team/honorouries/sophia-chen.jpg",
+  },
+  {
+    id: 17,
+    name: "Inaaya Rohail",
+    role: "Honoroury",
+    image: "/team/honorouries/inaaya-rohail.jpg",
+  },
+  {
+    id: 18,
+    name: "Manahil",
+    role: "Honoroury",
+    image: "/team/unknown.jpg",
+  },
+  {
+    id: 19,
+    name: "Yang Xue",
+    role: "Honoroury",
+    image: "/team/unknown.jpg",
+  },
+  {
+    id: 20,
+    name: "Rania Amin",
+    role: "Honoroury",
+    image: "/team/honorouries/rania-amin.jpg",
+  },
+  {
+    id: 21,
+    name: "Jhanvi Patel ",
+    role: "Honoroury",
+    image: "/team/unknown.jpg",
   },
 ];
 
@@ -157,11 +318,9 @@ const page = () => {
         {aboutTheTeam.map((about, index) => (
           <div
             key={index}
-            className="bg-gray-200 flex justify-center items-center flex-col text-center rounded-2xl h-[70vh] w-[40vh]"
+            className="bg-gray-200 flex justify-center items-center flex-col text-center rounded-2xl h-[90vh] w-[40vh]"
           >
-            <div
-              className="w-full flex justify-center items-center h-1/2"
-            >
+            <div className="w-full flex justify-center items-center h-1/3">
               <div className="w-52 h-52 overflow-hidden relative">
                 <Image
                   src={about.image ?? "/placeholder.jpg"}
@@ -196,6 +355,50 @@ const page = () => {
                 role={member.role}
                 image={member.image}
                 description={member.description}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* Grade reps sections */}
+      <div>
+        <div className="w-full flex flex-col justify-center items-center">
+          <div className="w-full flex justify-center items-center flex-col">
+            <p className="tracking-widest font-light">
+              Amplifying Our Voices and Keeping Us Connected...
+            </p>
+            <h1 className="font-medium text-4xl mt-4">Grade Representative</h1>
+          </div>
+          <div className="lg:grid lg:grid-cols-4 lg:gap-x-32 lg:gap-y-12 flex flex-col justify-center items-center gap-y-8 my-8">
+            {gradeReps.map((member) => (
+              <ExecutiveInfo
+                key={member.id}
+                name={member.name}
+                role={member.role}
+                image={member.image}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* Honorouries section */}
+      <div>
+        <div className="w-full flex flex-col justify-center items-center">
+          <div className="w-full flex justify-center items-center flex-col">
+            <p className="tracking-widest font-light">
+              Turning visions into reality with their support and dedication...
+            </p>
+            <h1 className="font-medium text-4xl mt-4">Honouraries</h1>
+          </div>
+          <div className="lg:grid lg:grid-cols-4 lg:gap-x-32 lg:gap-y-12 flex flex-col justify-center items-center gap-y-8 my-8">
+            {honouraries.map((member) => (
+              <ExecutiveInfo
+                key={member.id}
+                name={member.name}
+                role={member.role}
+                image={member.image}
               />
             ))}
           </div>
--- components/ExecutiveInfo.tsx
+++ components/ExecutiveInfo.tsx
@@ -8,7 +8,7 @@ interface ExecutiveInfoProps {
     name: string,
     role: string,
     image: string,
-    description: string,
+    description?: string,
 }
 
 const ExecutiveInfo: React.FC<ExecutiveInfoProps> = ({ name, role, image, description }) => {
@@ -48,37 +48,36 @@ const ExecutiveInfo: React.FC<ExecutiveInfoProps> = ({ name, role, image, descri
   };
 
   return (
-    <div>
-        <div className="relative inline-block">
-        <Image
-            ref={imageRef}
-            src={image}
-            width={100}
-            height={100}
-            alt="Executive"
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            className="w-40 h-40 rounded-full"
-        />
-            <div
-                ref={infoBoxRef}
-                className="absolute top-1/2 left-1/2 w-12 h-12 bg-black bg-opacity-80 text-white opacity-0 pointer-events-none transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center text-center p-2 box-border"
-            >
-                <div className='flex flex-col justify-evenly h-40'>
-                        <div>
-                            <h3 className="opacity-0 font-semibold text-xl">{name}</h3>
-                            <p className="opacity-0">{role}</p>
-                        </div>
-                        <p className="opacity-0">{description}</p>
-                </div>
-                
-            </div>
+    <div className="flex flex-col items-center justify-center">
+    <div className="relative inline-block">
+      <Image
+        ref={imageRef}
+        src={image}
+        width={100}
+        height={100}
+        alt="Executive"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className="w-40 h-40 rounded-full"
+      />
+      <div
+        ref={infoBoxRef}
+        className="absolute top-1/2 left-1/2 w-12 h-12 bg-black bg-opacity-80 text-white opacity-0 pointer-events-none transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center text-center p-2 box-border"
+      >
+        <div className="flex flex-col justify-evenly h-40">
+          <div>
+            <h3 className="opacity-0 font-semibold text-xl">{name}</h3>
+            <p className="opacity-0">{role}</p>
+          </div>
+          <p className="opacity-0">{description}</p>
         </div>
-        <div className='text-center'>
-            <h3 className='font-semibold text-xl'>{name}</h3>
-            <p>{role}</p>
-        </div>
+      </div>
     </div>
+    <div className="text-center mt-4">
+      <h3 className="font-semibold text-xl">{name}</h3>
+      <p>{role}</p>
+    </div>
+  </div>  
   );
 };
 
