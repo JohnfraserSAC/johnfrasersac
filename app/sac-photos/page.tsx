25a3569fec6a72f42fd6a1c9cb67e7f2e09cbf05
arya holmukhe <aryaholmukhe@gmail.com>
2024-12-08 01:14:58 -0500

    added photos

---
+++ app/sac-photos/page.tsx
@@ -0,0 +1,16 @@
+'use client'; // Ensures this component is client-side rendered.
+
+import React from 'react';
+import CountingAnimation from '@/components/Counting';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Page = () => {
+  return (
+    <main>
+    
+    </main>
+  );
+};
+
+export default Page;
