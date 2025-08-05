db1f5dfb055cbe158e0df174e4abe5c712d884da
YxngXi1 <xuetianyang@gmail.com>
2025-08-04 20:52:29 -0400

    fizxed all slug page to look prettier

--- app/announcements/all/[slug]/page.tsx
+++ app/announcements/all/[slug]/page.tsx
@@ -13,13 +13,17 @@ export default async function SingleAnnouncementPage({ params }: { params: { slu
   if (!announcement) return notFound();
 
   return (
-    <main className="container mx-auto py-12">
-      <h1 className="text-4xl font-bold mb-4">{announcement.title}</h1>
-      <div className="text-lg mb-4">
-        <ReactMarkdown >{announcement.description}</ReactMarkdown>
-      </div>
-      <p className="text-gray-600 text-sm">Date: {announcement.date}</p>
-      <p className="text-gray-600 text-sm">Club: {announcement.club}</p>
+    <main className="w-full">
+        <div className='text-white custom-background-4 flex justify-center items-center flex-col text-center pt-40 w-full py-8'>
+            <h1 className="text-4xl font-bold mb-4">{announcement.title}</h1>
+        </div>
+        <div className='py-12 mx-auto w-3/4 flex flex-col justify-center items-center text-center'>
+            <div className="text-lg mb-4">
+                <ReactMarkdown >{announcement.description}</ReactMarkdown>
+            </div>
+            <p className="text-gray-600 text-sm">Date: {announcement.date}</p>
+            <p className="text-gray-600 text-sm">Club: {announcement.club}</p>
+        </div>`
     </main>
   );
 }
